daf95ce469e245f39fc84e1b8cb82f2211bbaa75
SatyaNayak2051 <satyanayak2051@gmail.com>
2026-02-02 19:38:06 +0530

    'minor_mesage_change'

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8080;
 
 app.get("/", (req, res) => {
-  return res.json({ msg: "Hello from the server" });
+  return res.json({ msg: "Hello from the server1!!" });
 });
 
 app.listen(PORT, () => console.log(`App is up and running on ${PORT}`));
