b87586669df771bb439dda28f2707e2e796108f3
SatyaNayak2051 <satyanayak2051@gmail.com>
2026-02-02 15:04:54 +0530

    initial_commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT ?? 8080;
+
+app.get("/", (req, res) => {
+  return res.json({ msg: "Hello from the server" });
+});
+
+app.listen(PORT, () => console.log(`App is up and running on ${PORT}`));
