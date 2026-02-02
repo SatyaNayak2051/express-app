b9378c4c8ebe82ffe77586827ed15e16b03b29ea
SatyaNayak2051 <satyanayak2051@gmail.com>
2026-02-02 20:35:51 +0530

    'minor_change'

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT ?? 8080;
 
 app.get("/", (req, res) => {
   return res.json({
-    msg: "Hello from the server2 - deployed successfully hurrah!!",
+    msg: "Hello from the server2 - deployed successfully",
   });
 });
 
